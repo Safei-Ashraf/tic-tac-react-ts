d0d8d2be5493e836dba928ce1df7d843ff6cb837
Safei Ashraf <safei.ashraf@gmail.com>
2023-01-25 23:28:08 +0200

    feat: adding game logic and UI

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -2,21 +2,42 @@ import { useState } from "react";
 import { Square } from "../Square/Square";
 import "./Board.css";
 export const Board = () => {
-	const [board, setBoard] = useState(Array(9).fill("X"));
-	const resetBoard = () => {
-		setBoard((board) => board.fill(""));
-	};
+	const [board, setBoard] = useState(Array(9).fill(null));
+	const [xisNext, setXIsNext] = useState(true);
+	const [winner, setWinner] = useState("");
 
+	const resetBoard = () => {
+		setBoard((prevState) => (prevState = Array(9).fill(null)));
+	};
+	const handleClick = (index: number): void => {
+		const newBoard = board.slice();
+		if (xisNext) {
+			newBoard[index] = "X";
+			setXIsNext(false);
+		} else {
+			newBoard[index] = "O";
+			setXIsNext(true);
+		}
+		setBoard((board) => (board = newBoard));
+	};
 	return (
 		<div>
 			<button onClick={resetBoard}>Reset</button>
+			<p className="game-turns">
+				{winner.length === 0 && xisNext
+					? "Player X Turn "
+					: "Player O Turn"}
+			</p>
+			<p>{winner.length > 0 ? `Player ${winner} Won!` : ""}</p>
 			<div className="board-container">
 				{board.map((sqr, index) => {
 					return (
 						<Square
 							val={sqr}
 							key={index}
-							onClick={() => console.log(sqr, index)}
+							onClick={() => {
+								handleClick(index);
+							}}
 						/>
 					);
 				})}
