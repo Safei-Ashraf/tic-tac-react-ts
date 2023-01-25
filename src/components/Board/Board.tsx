14ec560d9aa758ee660cd3f99d1458084a9a1043
Safei Ashraf <safei.ashraf@gmail.com>
2023-01-25 23:38:17 +0200

    feat: finalize game logic

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -3,15 +3,22 @@ import { Square } from "../Square/Square";
 import "./Board.css";
 export const Board = () => {
 	const [board, setBoard] = useState(Array(9).fill(null));
-	const [xisNext, setXIsNext] = useState(true);
+	const [xIsNext, setXIsNext] = useState(true);
 	const [winner, setWinner] = useState("");
+	const gameWinner = calculateWinner(board as []);
+	let status;
+	if (gameWinner) {
+		status = `Winner is ${gameWinner}`;
+	} else {
+		status = "Next player: " + (xIsNext ? "X" : "O");
+	}
 
 	const resetBoard = () => {
 		setBoard((prevState) => (prevState = Array(9).fill(null)));
 	};
 	const handleClick = (index: number): void => {
 		const newBoard = board.slice();
-		if (xisNext) {
+		if (xIsNext) {
 			newBoard[index] = "X";
 			setXIsNext(false);
 		} else {
@@ -22,13 +29,7 @@ export const Board = () => {
 	};
 	return (
 		<div>
-			<button onClick={resetBoard}>Reset</button>
-			<p className="game-turns">
-				{winner.length === 0 && xisNext
-					? "Player X Turn "
-					: "Player O Turn"}
-			</p>
-			<p>{winner.length > 0 ? `Player ${winner} Won!` : ""}</p>
+			<p>{status && <span>{status}</span>}</p>
 			<div className="board-container">
 				{board.map((sqr, index) => {
 					return (
@@ -42,6 +43,33 @@ export const Board = () => {
 					);
 				})}
 			</div>
+			<button className="reset-button" onClick={resetBoard}>
+				Reset
+			</button>
 		</div>
 	);
 };
+
+function calculateWinner(squares: []) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (
+			squares[a] &&
+			squares[a] === squares[b] &&
+			squares[a] === squares[c]
+		) {
+			return squares[a];
+		}
+	}
+	return null;
+}
